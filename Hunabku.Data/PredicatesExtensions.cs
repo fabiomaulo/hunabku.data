75a9df919f5fbf35c4ccc439136f9de282ec5b2c
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 14:12:51 -0300

    WhereAndClause

---
+++ Hunabku.Data/PredicatesExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Hunabku.Data
+{
+	public static class PredicatesExtensions
+	{
+		public static IEnumerable<QueryParameter> DistinctByName(this IEnumerable<QueryParameter> source)
+		{
+			if (source == null) throw new ArgumentNullException("source");
+			return source.Where(x => x != null).Distinct(new DbParameterNameEqualityComparar());
+		}
+
+		public static IEnumerable<IDbDataParameter> ToDbParameters(this IEnumerable<QueryParameter> source, IDbCommand command)
+		{
+			if (source == null) throw new ArgumentNullException("source");
+			if (command == null) throw new ArgumentNullException("command");
+			return source.Where(x => x != null).Select(x => x.ToDbParameter(command));
+		}
+
+		public static IDbDataParameter ToDbParameter(this QueryParameter source, IDbCommand command)
+		{
+			if (source == null) throw new ArgumentNullException("source");
+			if (command == null) throw new ArgumentNullException("command");
+			var parameter = command.CreateParameter();
+			parameter.ParameterName = source.ParameterName;
+			parameter.Value = source.Value;
+			return parameter;
+		}
+
+		private class DbParameterNameEqualityComparar : IEqualityComparer<QueryParameter>
+		{
+			public bool Equals(QueryParameter x, QueryParameter y)
+			{
+				return Equals(x.ParameterName, y.ParameterName);
+			}
+
+			public int GetHashCode(QueryParameter obj)
+			{
+				return obj.ParameterName.GetHashCode();
+			}
+		}
+	}
+}
---
+++ Hunabku.Data/WhereAndClause.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Hunabku.Data.Predicates;
+
+namespace Hunabku.Data
+{
+	public class WhereAndClause : IWhereClause
+	{
+		private readonly List<IWherePredicate> whereAndClauses = new List<IWherePredicate>();
+
+		public void And(string predicate, QueryParameter parameterValue)
+		{
+			whereAndClauses.Add(new PredicateWithParameter { Predicate = predicate, Parameter = parameterValue });
+		}
+
+		public void AndIn(string fieldPath, object[] values)
+		{
+			whereAndClauses.Add(new InPredicate { FieldPath = fieldPath, Values = values });
+		}
+
+		public void And(string predicate)
+		{
+			whereAndClauses.Add(new PredicateOnly { Predicate = predicate });
+		}
+
+		public void And(string predicate, IEnumerable<QueryParameter> parametersValue)
+		{
+			whereAndClauses.Add(new PredicateWithParameters { Predicate = predicate, Parameters = parametersValue });
+		}
+
+		public void And(IWhereClause predicate)
+		{
+			whereAndClauses.Add(new PredicateClause { Clause = predicate });
+		}
+
+		public string Clause()
+		{
+			return string.Join(" and ", whereAndClauses.Select(x => x.Predicate()).Where(x => !string.IsNullOrEmpty(x)).ToArray());
+		}
+
+		public IEnumerable<QueryParameter> Parameters()
+		{
+			return whereAndClauses
+				.SelectMany(x => x.Parameters()).DistinctByName();
+		}
+
+		public IEnumerable<IDbDataParameter> Parameters(IDbCommand command)
+		{
+			return Parameters().ToDbParameters(command);
+		}
+
+		public override string ToString()
+		{
+			return Clause();
+		}
+	}
+}
