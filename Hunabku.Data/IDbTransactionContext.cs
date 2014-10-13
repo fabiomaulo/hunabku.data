92ee9f203ce5e9cc5ae812c380dcba2f806a7285
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 14:25:39 -0300

    Interfaces para manejo de transacciones

---
+++ Hunabku.Data/IDbTransactionContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+
+namespace Hunabku.Data
+{
+	/// <summary>
+	/// Contexto de ejecución de commands a RDBMS.
+	/// </summary>
+	/// <remarks>
+	/// Los usuarios de esta implementación pueden leer sea la connection que la transaction pero
+	/// no deberían jamás operar sobre ellas cambiando su estado (ni close y menos dispose).
+	/// </remarks>
+	public interface IDbTransactionContext : IDisposable
+	{
+		IDbConnection Connection { get; }
+		IDbTransaction Transaction { get; }
+
+		/// <summary>
+		/// Crea un nuevo see <see cref="IDbCommand"/> asociado con la <see cref="Connection"/> y la <see cref="Transaction"/>.
+		/// </summary>
+		/// <returns>Un nuevo <see cref="IDbCommand"/>.</returns>
+		IDbCommand CreateCommand();
+
+		/// <summary>
+		/// Define si el context se está ejecuntando en un marco readonly. En tal caso no se efectuarán modificaciones al DB.
+		/// </summary>
+		bool IsReadOnly { get; }
+	}
+}
---
+++ Hunabku.Data/IDbTransactionContextProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace Hunabku.Data
+{
+	/// <summary>
+	/// Provee el transaction context en el marco del cual se pueden ejecutar una serie de <see cref="IDbCommand"/>.
+	/// </summary>
+	/// <remarks>
+	/// En general una clase de servicio puede crear un <see cref="IDbTransactionContext"/> en el marco del cual
+	/// se ejecutan todos los acceso a DB que el metodo del servicio mismo necesita.
+	/// Tambien, para commands que se ejecutan asincronico en su propio thread (un thread por action) será posible
+	/// implementar un <see cref="IDbTransactionContextProvider"/> para el entero thread así como es posible
+	/// un <see cref="IDbTransactionContextProvider"/> per request context (en WEB).
+	/// </remarks>
+	public interface IDbTransactionContextProvider
+	{
+		IDbTransactionContext GetCurrentContext();
+	}
+}
