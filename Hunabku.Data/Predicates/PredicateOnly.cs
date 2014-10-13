2c0808d220261752daab852f176857466d7f656f
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 13:54:53 -0300

    predicate only

---
+++ Hunabku.Data/Predicates/PredicateOnly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Hunabku.Data.Predicates
+{
+	/// <summary>
+	/// Predicato sin parametros.
+	/// </summary>
+	/// <example>
+	/// "t1.Field1 = t2.Field1"
+	/// </example>
+	public class PredicateOnly : IWherePredicate
+	{
+		public string Predicate { get; set; }
+
+		string IWherePredicate.Predicate()
+		{
+			return string.IsNullOrEmpty(Predicate) ? "" : string.Concat("(", Predicate, ")");
+		}
+
+		public IEnumerable<QueryParameter> Parameters()
+		{
+			yield break;
+		}
+	}
+}
