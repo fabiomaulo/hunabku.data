e9c2602c842681ed0c23ddc030df6dd7b198a3fe
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 14:07:14 -0300

    Predicate multiparametros

---
+++ Hunabku.Data/Predicates/PredicateWithParameters.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hunabku.Data.Predicates
+{
+	/// <summary>
+	/// Predicate multi parametros
+	/// </summary>
+	public class PredicateWithParameters : IWherePredicate
+	{
+		public string Predicate { get; set; }
+		public IEnumerable<QueryParameter> Parameters { get; set; }
+
+		string IWherePredicate.Predicate()
+		{
+			return string.IsNullOrEmpty(Predicate) ? "" : string.Concat("(", Predicate, ")");
+		}
+
+		IEnumerable<QueryParameter> IWherePredicate.Parameters()
+		{
+			return Parameters ?? Enumerable.Empty<QueryParameter>();
+		}
+	}
+}
