4ad6b77e018f3d1cdd8f23e12f6d88863b61414e
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 14:09:39 -0300

    Predicate clause

---
+++ Hunabku.Data/Predicates/PredicateClause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Hunabku.Data.Predicates
+{
+	/// <summary>
+	/// Predicato que incluye una clausola where completa
+	/// </summary>
+	/// <remarks>
+	/// Se usa cuando la clausola where incluye operaciones con AND y OR
+	/// </remarks>
+	public class PredicateClause : IWherePredicate
+	{
+		public IWhereClause Clause { get; set; }
+
+		public string Predicate()
+		{
+			return string.Concat("(", Clause.Clause(), ")");
+		}
+
+		public IEnumerable<QueryParameter> Parameters()
+		{
+			return Clause.Parameters();
+		}
+	}
+}
