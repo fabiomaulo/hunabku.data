476a41b22632578fa5cd4e4a71d69b6e5ec171a4
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 12:04:32 -0300

    Connection a SQL server

---
+++ Hunabku.Data/ConnectionProviderFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hunabku.Data
+{
+	public class ConnectionProviderFactory
+	{
+		private static IConnectionProvider current;
+		private static readonly object syncObj = new object();
+
+		/// <summary>
+		/// Connection provider común a toda la applicación.
+		/// </summary>
+		/// <remarks>
+		///  Si se usa el current se asume que no hay diferentes consfiguraciones por thread.
+		/// </remarks>
+		public static IConnectionProvider Current
+		{
+			get { return current; }
+			set
+			{
+				var prev = current;
+				lock (syncObj)
+				{
+					if (prev != null && value != null)
+					{
+						throw new InvalidOperationException("No es posible cambiar la configuración del connection provider.");
+					}
+				}
+				current = value;
+			}
+		}
+
+		public static IConnectionProvider NewConnectionProvider(IDictionary<string, string> settings)
+		{
+			/* 
+			 * En este caso solo nos conectamos via SQLServer así que el connectionProvider es bien conocido.
+			 * Desde los settings se podría conocer la clase concreta de IConnectionProvider, instanciarla con el Activator
+			 * y luego usarla en el programa.
+			*/
+			IConnectionProvider connections = new SqlConnectionProvider();
+			connections.Configure(settings);
+			return connections;
+		} 
+	}
+}
---
+++ Hunabku.Data/SqlConnectionProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Hunabku.Data
+{
+	public class SqlConnectionProvider: IConnectionProvider
+	{
+		private string connString;
+
+		public void Configure(IDictionary<string, string> settings)
+		{
+			// cualquier dato se necesite configurar se puede pasar en settings que a su vez puede venir desde cualquier lado
+			// desde el app.config, los settings de la aplicación o del usuario, desde un string fijo en RAM etc.
+			connString = "Connection String No Configurada";
+			string connectionStringName;
+			if (settings.TryGetValue("ConnectionStringName", out connectionStringName))
+			{
+				var conString = (from ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings
+												 where connectionStringName.Equals(connectionString.Name)
+												 select connectionString.ConnectionString).FirstOrDefault();
+				if (conString != null)
+				{
+					var connectionStringBuilder = new SqlConnectionStringBuilder(conString);
+					connString = connectionStringBuilder.ConnectionString;
+				}
+			}
+		}
+		protected virtual string ConnectionString
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(connString))
+				{
+					throw new InvalidOperationException("El string de conexión con el WebGet no fue configurado.");
+				}
+				return connString;
+			}
+		}
+
+		public IDbConnection GetConnection()
+		{
+			IDbConnection conn = new SqlConnection();
+			try
+			{
+				conn.ConnectionString = ConnectionString;
+				conn.Open();
+			}
+			catch (Exception)
+			{
+				conn.Dispose();
+				throw;
+			}
+
+			return conn;
+		}
+	}
+}
