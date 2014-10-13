c86ab6cfc68cd3c4b189cfe8786fa9642946831b
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 13:58:59 -0300

    Predicate con parametro e InPredicate

---
+++ Hunabku.Data/Predicates/InPredicate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Hunabku.Data.Predicates
+{
+	public class InPredicate : IWherePredicate
+	{
+		public string FieldPath { get; set; }
+		public object[] Values { get; set; }
+
+		public string Predicate()
+		{
+			if (Values == null || Values.Length == 0)
+			{
+				return "";
+			}
+			bool esString = Values[0] is string;
+			var sb = new StringBuilder();
+			sb.Append('(');
+			if (Values.Length == 1)
+			{
+				sb.Append(FieldPath);
+				sb.Append(" = ");
+				sb.Append(GetSqlValue(esString, Values[0]));
+			}
+			else
+			{
+				sb.Append(FieldPath);
+				sb.Append(" in (");
+				sb.Append(string.Join(", ", Values.Select(x => GetSqlValue(esString, x)).ToArray()));
+				sb.Append(')');
+			}
+
+			sb.Append(')');
+			return sb.ToString();
+		}
+
+		public IEnumerable<QueryParameter> Parameters()
+		{
+			yield break;
+		}
+
+		private string GetSqlValue(bool esString, object value)
+		{
+			return esString
+				? string.Concat("'", value, "'")
+				: Convert.ToString(value, CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ Hunabku.Data/Predicates/PredicateWithParameter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Hunabku.Data.Predicates
+{
+	/// <summary>
+	/// Predicato con un parametro.
+	/// </summary>
+	/// <example>
+	/// "t1.Field1 = @param1"
+	/// </example>
+	public class PredicateWithParameter : IWherePredicate
+	{
+		public string Predicate { get; set; }
+		public QueryParameter Parameter { get; set; }
+
+		string IWherePredicate.Predicate()
+		{
+			return string.IsNullOrEmpty(Predicate) ? "" : string.Concat("(", Predicate, ")");
+		}
+
+		public IEnumerable<QueryParameter> Parameters()
+		{
+			yield return Parameter;
+		}
+	}
+}
