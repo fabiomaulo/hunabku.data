687058edd2d571cdc08cde2275d5c95ebe7ec02e
Fabio Maulo <fabiomaulo@gsh.la>
2014-10-13 11:44:22 -0300

    IConnectionProvider

---
+++ Hunabku.Data/IConnectionProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace Hunabku.Data
+{
+	public interface IConnectionProvider
+	{
+		/// <summary>
+		/// Initialize the connection provider from the given properties.
+		/// </summary>
+		/// <param name="settings">The connection provider settings</param>
+		void Configure(IDictionary<string, string> settings);
+
+		/// <summary>
+		/// Get an open <see cref="IDbConnection"/>.
+		/// </summary>
+		/// <returns>An open <see cref="IDbConnection"/>.</returns>
+		IDbConnection GetConnection(); 
+	}
+}
